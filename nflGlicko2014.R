fb2fdec5cf38a7e08943e1e2750d2cc866b2b3f6
jscarlton <stuart@jscarlton.net>
2015-06-30 16:42:50 -0500

    Initial 2014 glicko ratings
    Computed the initial Glicko analysis for 2014. I didn’t use any input
    from prior years to tune the ratings. Nor did I set a gamma parameter,
    which I’ll need to optimize at some point.

--- nflGlicko2014.R
+++ nflGlicko2014.R
@@ -1,7 +1,7 @@
-# Project: NFL Glicko Analysis
-# File: nflGlicko2014.r
-# Date: 2015-06-30
-# Task: Initial Glicko ratings for 2014 NFL data
+# Project:      NFL Glicko Analysis
+# File:         nflGlicko2014.r
+# Date created: 2015-06-30
+# Task:         Initial Glicko ratings for 2014 NFL data
 library(PlayerRatings)
 
 # Import the 2014 NFL data. Will do other data later.
@@ -26,3 +26,23 @@ nfl2014Glicko <- nfl2014[,c(2,4,7,8)]
 # Glicko analysis for the full 2014 season
 nflGlicko <- glicko(nfl2014Glicko)
 nflGlicko
+
+# Week-by-week Glicko analysis for 2014 using the prior week's ratings as the status. No gamma setting for now: will need to optimize that later. When I do prior seasons, can also use it for the status input for week 1. Or maybe that + some regression to the mean factor.
+glicko20141 <- glicko(nfl2014Glicko[nfl2014Glicko$week == 1, ])
+glicko20142 <- glicko(nfl2014Glicko[nfl2014Glicko$week == 2, ], status = glicko20141$ratings)
+glicko20143 <- glicko(nfl2014Glicko[nfl2014Glicko$week == 3, ], status = glicko20142$ratings)
+glicko20144 <- glicko(nfl2014Glicko[nfl2014Glicko$week == 4, ], status = glicko20143$ratings)
+glicko20145 <- glicko(nfl2014Glicko[nfl2014Glicko$week == 5, ], status = glicko20144$ratings)
+glicko20146 <- glicko(nfl2014Glicko[nfl2014Glicko$week == 6, ], status = glicko20145$ratings)
+glicko20147 <- glicko(nfl2014Glicko[nfl2014Glicko$week == 7, ], status = glicko20146$ratings)
+glicko20148 <- glicko(nfl2014Glicko[nfl2014Glicko$week == 8, ], status = glicko20147$ratings)
+glicko20149 <- glicko(nfl2014Glicko[nfl2014Glicko$week == 9, ], status = glicko20148$ratings)
+glicko201410 <- glicko(nfl2014Glicko[nfl2014Glicko$week == 10, ], status = glicko20149$ratings)
+glicko201411 <- glicko(nfl2014Glicko[nfl2014Glicko$week == 11, ], status = glicko201410$ratings)
+glicko201412 <- glicko(nfl2014Glicko[nfl2014Glicko$week == 12, ], status = glicko201411$ratings)
+glicko201413 <- glicko(nfl2014Glicko[nfl2014Glicko$week == 13, ], status = glicko201412$ratings)
+glicko201414 <- glicko(nfl2014Glicko[nfl2014Glicko$week == 14, ], status = glicko201413$ratings)
+glicko201415 <- glicko(nfl2014Glicko[nfl2014Glicko$week == 15, ], status = glicko201414$ratings)
+glicko201416 <- glicko(nfl2014Glicko[nfl2014Glicko$week == 16, ], status = glicko201415$ratings)
+glicko201417 <- glicko(nfl2014Glicko[nfl2014Glicko$week == 17, ], status = glicko201416$ratings)
+# Merge into a grand 2014 dataset
