3522d47cf741b4bc84984c882a0200e87c4f640a
jscarlton <stuart@jscarlton.net>
2015-06-30 09:22:52 -0500

    Initial analysis

---
+++ nflGlicko2014.R
@@ -0,0 +1,28 @@
+# Project: NFL Glicko Analysis
+# File: nflGlicko2014.r
+# Date: 2015-06-30
+# Task: Initial Glicko ratings for 2014 NFL data
+library(PlayerRatings)
+
+# Import the 2014 NFL data. Will do other data later.
+nfl2014 <- read.csv("./nfl_results/nfl 2014.csv", header = TRUE)
+
+# PlayerRatings asks for a dataframe with:
+#   (1) a numeric vector denoting the time period in which the game took place
+#   (2) a numeric or character identifier for player one
+#   (3) a numeric or character identifier for player two and
+#   (4) the result of the game expressed as a number, typically equal to one for a player one win, zero for a player two win and one half for a draw.
+
+# Calculate the results column.
+nfl2014$result <- ifelse(nfl2014$home_score>nfl2014$visitors_score, 1, ifelse(nfl2014$home_score<nfl2014$visitors_score,0,0.5))
+
+# Convert the home team and away team to characters
+nfl2014$home_team <- as.character(nfl2014$home_team)
+nfl2014$visiting_team <- as.character(nfl2014$visiting_team)
+
+# Create the dataframe for the glicko analysis
+nfl2014Glicko <- nfl2014[,c(2,4,7,8)]
+
+# Glicko analysis for the full 2014 season
+nflGlicko <- glicko(nfl2014Glicko)
+nflGlicko
